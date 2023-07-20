de0bb7854dd47a81213c1070b9ca19b8bd13a038
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-07-20 16:11:36 +0530

    this commit for the checkbox save update and its view and make the set_student method

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,15 +1,12 @@
 class SystemsController < ApplicationController
+  before_action :set_system, only: %i[show edit update destroy]
   def index
     @systems = System.all
   end
-
   def new
     @system = System.new
   end
-  
-
   def create
-    @system = System.new(system_params)
     @system.fav_system = params[:fav_system]
     if @system.save
       redirect_to @system, notice: 'System was successfully created.'
@@ -17,33 +14,28 @@ class SystemsController < ApplicationController
       render :new
     end
   end
- 
   def show 
-    @system =System.find(params[:id])
- 
-   end 
-   def edit
-    @system =System.find(params[:id])
- 
-   end
-   def update
-    @system = System.find(params[:id])
+  end 
+  def edit
+  end
+  def update
+    @system.fav_system = params[:fav_system]
     if @system.update(system_params)
       redirect_to system_path(@system)
     else
       render :edit
     end
   end
-   def destroy
-
-
-
-   end
-  # ...
-
+  def destroy
+    @system.destroy
+    redirect_to systems_path
+  end
   private
-
   def system_params
     params.require(:system).permit(:s_name, :s_year)
+  end
+
+  def set_system
+    @system = System.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :employees
   resources :users
   resources :systems
+  delete '/systems/:id', to: 'systems#destroy', as: 'destroy_system'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
